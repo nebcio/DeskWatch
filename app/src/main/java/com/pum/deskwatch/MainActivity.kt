3f231b53b17817376bfb03ad279d0ea77a3761c0
nebcio <324930@uwr.edu.pl>
2023-02-04 19:25:06 +0100

    add: dark mode

--- app/src/main/java/com/pum/deskwatch/MainActivity.kt
+++ app/src/main/java/com/pum/deskwatch/MainActivity.kt
@@ -2,10 +2,26 @@ package com.pum.deskwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.pum.deskwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                as NavHostFragment
+        navHostFragment.findNavController()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        //binding.//.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/pum/deskwatch/MainFragment.kt
+++ app/src/main/java/com/pum/deskwatch/MainFragment.kt
@@ -1,6 +1,57 @@
 package com.pum.deskwatch
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import com.pum.deskwatch.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
+    private lateinit var binding: MainFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val activity: AppCompatActivity = activity as AppCompatActivity
+
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val themeSwitch: Switch = binding.themeSwitch
+        // listener for switch
+        themeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+
+//        view.findViewById<RadioGroup>(R.id.radioButtonTheme).setOnCheckedChangeListener { _, checkedId ->
+//            when(checkedId){
+//                R.id.radioButtonLight -> {
+//                    themeTextView.text = getString(R.string.light_theme)
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+//                }
+//                R.id.radioButtonDark -> {
+//                    themeTextView.text = getString(R.string.dark_theme)
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+//                }
+//                R.id.radioButtonDefault -> {
+//                    themeTextView.text = getString(R.string.current_theme)
+//                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+//                }
+//            }
+    }
 }
